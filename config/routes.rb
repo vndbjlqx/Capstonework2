a05eb3610f134765a278e684bd5dd0f0ec8279ee
vndbjlqx <iamlqx@126.com>
2017-07-03 14:11:29 +0800

    set production site to under construction

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   
   scope :api, defaults: {format: :json} do
   	#resources :foos
-  	resources :foos, except: [:new, :edit]
-  	resources :bars, except: [:new, :edit]
-	resources :cities, except: [:new, :edit]
-	resources :states, except: [:new, :edit]
+  	# resources :foos, except: [:new, :edit]
+  	# resources :bars, except: [:new, :edit]
+	  # resources :cities, except: [:new, :edit]
+	  # resources :states, except: [:new, :edit]
   end
   
   get '/ui' => 'ui#index'
